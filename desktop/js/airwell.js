816c652a6c72a97bf9e45aaf9f97afb2acb85f19
Clément Cunin <clement-cunin@users.noreply.github.com>
2026-06-05 10:32:05 +0200

    fix: make addCmdToTable global so Jeedom core can call it

--- desktop/js/airwell.js
+++ desktop/js/airwell.js
@@ -1,7 +1,6 @@
 "use strict";
 
 $(() => {
-    // Load equipment list on page ready
     $("#bt_bindDevice").off("click").on("click", () => {
         const eqLogicId = $(".eqLogicAttr[data-l1key='id']").val();
         if (!eqLogicId) {
@@ -25,47 +24,44 @@ $(() => {
                     return;
                 }
                 $.fn.showAlert({ message: "{{Binding réussi}}", level: "success" });
-                // Refresh page to show the new key
                 $(".eqLogicAction[data-action='getInfo']").trigger("click");
             },
         });
     });
-
-    // Commands table rendering
-    function addCmdToTable(_cmd) {
-        if (!isset(_cmd)) _cmd = {};
-        if (!isset(_cmd.configuration)) _cmd.configuration = {};
-        const tr = $("<tr>")
-            .attr({ "data-cmd_id": init(_cmd.id) })
-            .addClass("cmd");
-        tr.append(
-            $("<td>").append(
-                $("<input>")
-                    .addClass("cmdAttr form-control input-sm")
-                    .attr({ "data-l1key": "name" })
-                    .val(init(_cmd.name))
-            )
-        );
-        tr.append(
-            $("<td>").text(init(_cmd.type) + " / " + init(_cmd.subType))
-        );
-        tr.append($("<td>"));
-        tr.append(
-            $("<td>").append(
-                $("<a>")
-                    .addClass("btn btn-default btn-xs cmdAction")
-                    .attr({ "data-action": "configure" })
-                    .html('<i class="fas fa-cogs"></i>')
-            ).append(" ").append(
-                $("<a>")
-                    .addClass("btn btn-default btn-xs cmdAction")
-                    .attr({ "data-action": "test" })
-                    .html('<i class="fas fa-rss"></i> {{Tester}}')
-            )
-        );
-        $("#table_cmd tbody").append(tr);
-        tr.setValues(_cmd, ".cmdAttr");
-    }
-
-    jeedom.eqLogic.builSelectCmd = addCmdToTable;
 });
+
+// Must be global — Jeedom core calls this to render each command row in the table
+function addCmdToTable(_cmd) {
+    if (!isset(_cmd)) _cmd = {};
+    if (!isset(_cmd.configuration)) _cmd.configuration = {};
+    const tr = $("<tr>")
+        .attr({ "data-cmd_id": init(_cmd.id) })
+        .addClass("cmd");
+    tr.append(
+        $("<td>").append(
+            $("<input>")
+                .addClass("cmdAttr form-control input-sm")
+                .attr({ "data-l1key": "name" })
+                .val(init(_cmd.name))
+        )
+    );
+    tr.append(
+        $("<td>").text(init(_cmd.type) + " / " + init(_cmd.subType))
+    );
+    tr.append($("<td>"));
+    tr.append(
+        $("<td>").append(
+            $("<a>")
+                .addClass("btn btn-default btn-xs cmdAction")
+                .attr({ "data-action": "configure" })
+                .html('<i class="fas fa-cogs"></i>')
+        ).append(" ").append(
+            $("<a>")
+                .addClass("btn btn-default btn-xs cmdAction")
+                .attr({ "data-action": "test" })
+                .html('<i class="fas fa-rss"></i> {{Tester}}')
+        )
+    );
+    $("#table_cmd tbody").append(tr);
+    tr.setValues(_cmd, ".cmdAttr");
+}
